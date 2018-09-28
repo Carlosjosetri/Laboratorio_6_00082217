8d85b051ec0442bdac613dd7994d1e0110beaf1f
carlosjosetri <00082217@uca.edu.sv>
2018-09-28 02:53:27 -0600

    initial commit

--- Ejercicio_6.js
+++ Ejercicio_6.js
@@ -1,7 +1,12 @@
-var abs = prompt("Ingrese un numero: ", "#")
-if(abs>0){
-    console.log(abs);
+function encri(array)
+{ var numero=[0,1,2,3,4,5,6,7,8,9]
+    var clave=["m","u","r","c","i","e","l","a","g","o"];
+	for (var i=0;i<array.length;i++){
+      if(clave.includes(array[i])){
+          array[i]=numero[clave.indexOf(array[i])]
+      }
+
 }
-else{
-    console.log(abs*-1);
+    console.log(array);
+  
 }
